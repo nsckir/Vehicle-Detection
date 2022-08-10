e3795c2e7ed5c38a3542b9a1537bfc0c36300750
kiril <kiril.schewzow@gmail.com>
2022-08-10 15:12:23 +0200

    Add error bars to the chart

--- app/run.py
+++ app/run.py
@@ -5,8 +5,8 @@ import plotly
 from flask import Flask
 from flask import render_template, request
 from plotly.graph_objs import Pie
+from plotly.graph_objs import Bar
 
-from plotly.graph_objs import Scatter
 from sqlalchemy import create_engine
 import os
 app = Flask(__name__)
@@ -30,9 +30,11 @@ def index():
     detected_objects['date'] = pd.to_datetime(detected_objects['date'])
     object_types = ['car', 'motorcycle', 'bus', 'truck']
     detected_objects['total'] = detected_objects[object_types].sum(axis=1)
-    num_obj_by_hour = detected_objects.groupby(detected_objects['date'].dt.hour)['total'].mean().reset_index()
+    num_obj_by_hour = detected_objects.groupby(detected_objects['date'].dt.hour).agg({'total': ['mean', 'std']})
+    num_obj_by_hour.columns = num_obj_by_hour.columns.droplevel()
+    num_obj_by_hour.reset_index(inplace=True)
     num_obj_by_hour = num_obj_by_hour.rename(columns={'date': 'hour'})
-    num_obj_by_hour['hour'] = num_obj_by_hour['hour'] - 6
+    num_obj_by_hour['hour'] = (num_obj_by_hour['hour'] - 6) % 24
 
     obj_by_type = detected_objects[object_types].sum().reset_index()
     obj_by_type.columns = ['vehicle_type', 'count']
@@ -41,9 +43,13 @@ def index():
     graphs = [
         {
             'data': [
-                Scatter(
+                Bar(
                     x=num_obj_by_hour['hour'],
-                    y=num_obj_by_hour['total']
+                    y=num_obj_by_hour['mean'],
+                    error_y=dict(
+                        type='data',  # value of error bar given in data coordinates
+                        array=num_obj_by_hour['std'],
+                        visible=True)
                 )
             ],
 
@@ -53,8 +59,10 @@ def index():
                     'title': "Average number of vehicles in the image"
                 },
                 'xaxis': {
-                    'title': "Hour"
-                }
+                    'title': "Hour",
+                    'range': [-0.5, 23.5]
+                },
+
             }
         },
 
@@ -63,22 +71,13 @@ def index():
                 Pie(
                     labels=obj_by_type['vehicle_type'],
                     values=obj_by_type['count'],
-                    # mode='markers',
-                    # hovertext=imbalance.index
-
                 )
             ],
 
             'layout': {
-                'title': 'Correlation of category imbalance and f1 test score',
-                'yaxis': {
-                    'title': "f1 test score"
-                },
-                'xaxis': {
-                    'title': "imbalance (majority class/minority class)"
-                }
+                'title': 'Detected Vehicle Types',
             }
-        }
+        },
     ]
 
     # encode plotly graphs in JSON
@@ -90,7 +89,7 @@ def index():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(host='0.0.0.0', port=3001, debug=False)
 
 
 if __name__ == '__main__':
